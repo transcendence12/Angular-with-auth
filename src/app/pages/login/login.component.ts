669a348e941543e9983d1c9a200f7c0fd49fcd71
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2023-08-11 20:08:20 +0200

    in auth service httpClient imported

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
