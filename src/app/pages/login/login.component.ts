83069d7bc39ce90bcbdd8b48a50a03cc3b05ca85
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2023-08-11 18:38:52 +0200

    login form created

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  form = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+  error = ''
 
+  constructor(private fb: FormBuilder) {}
+  
+    onSubmit(){
+      console.log("submit: ", this.form.value)
+    }
+
+    createAccount(){
+      console.log("Creare: ", this.form.value)
+    }
 }
