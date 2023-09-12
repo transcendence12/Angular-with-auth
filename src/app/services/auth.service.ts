c9926272ff475775ad3ebc1c0fe77bf5a21e3dc8
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2023-09-12 13:01:13 +0200

    login and register service created but for now type user doesnt match with response type; it's for debuging

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import jwtDecode, { JwtPayload } from 'jwt-decode';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
+export const USER_STORAGE_KEY = 'APP_TOKEN';
 
 interface User {
   email: string;
@@ -13,32 +16,89 @@ interface RegisteredUser {
   email: string;
 }
 
-interface ResponseData {
+interface ResponseUserData {
   token: string;
-  registeredUser: RegisteredUser;
+  id: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 // 'https://reqres.in/api/register'
 export class AuthService {
+  private user: BehaviorSubject<ResponseUserData | null | undefined> =
+    new BehaviorSubject<ResponseUserData | null | undefined>(undefined);
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadUser()
+  }
+  loadUser() {
+    const token = localStorage.getItem(USER_STORAGE_KEY)
 
-  login(email:string, password:string):Observable<User>{
-    return this.http.post<User>('http://localhost:3000/register', {
-      email,
-      password,
-    }).pipe(
-      map((res: any) => {
-        console.log("Result ", res);
+    if(token) {
+      const decoded = jwtDecode<JwtPayload>(token);
+      console.log('auth', decoded);
+      const responseUserData: ResponseUserData = {
+        token: token,
+        id: decoded.sub!,
+      };
+      this.user.next(responseUserData);
+      
+    } else {
+      this.user.next(null);
+    }
+  }
 
-        return res;
+
+  register(email: string, password: string): Observable<User> {
+    return this.http
+      .post<User>('http://localhost:3000/users', {
+        email,
+        password,
       })
+      .pipe(
+        switchMap((res:any) => {
+          return this.login(email, password)
+        })
+      )
+  }
 
-    );}
+
+  login(email: string, password: string): Observable<User> {
+    return this.http
+      .post<User>('http://localhost:3000/register', {
+        email,
+        password,
+      })
+      .pipe(
+        map((res: any) => {
+          console.log('Result ', res);
+          localStorage.setItem(USER_STORAGE_KEY, res.token);
+          const decoded = jwtDecode<JwtPayload>(res.token);
+          console.log('auth', decoded);
+          const responseUserData: ResponseUserData = {
+            token: res.token,
+            id: decoded.sub!,
+          };
+          this.user.next(responseUserData);
+          return responseUserData;
+        })
+      );
+  }
+
+  signOut(){
+    localStorage.removeItem(USER_STORAGE_KEY);
+    this.user.next(null);
+  }
+
+  getCurrentUser(){
+    return this.user.asObservable();
+  }
+
+  getCurrentUserId(){
+    return this.user.getValue()!.id;
+  }
 }
 
 // This is the respons from json-server-auth:
@@ -49,4 +109,3 @@ export class AuthService {
 //     "id": 5
 //   }
 // }
-
